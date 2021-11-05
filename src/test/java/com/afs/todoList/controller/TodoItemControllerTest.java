ae9379caf7b622342bc88671375547cb418b5be0
Tommy Cheung <tommy200401@gmail.com>
2021-11-05 09:52:31 +0800

    [TodoList][Tommy] test: reset ID before each test

--- src/test/java/com/afs/todoList/controller/TodoItemControllerTest.java
+++ src/test/java/com/afs/todoList/controller/TodoItemControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
@@ -17,6 +18,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Sql(statements = "alter table todo_item alter column id restart with 1")
 @SpringBootTest
 @AutoConfigureMockMvc
 class TodoItemControllerTest {
